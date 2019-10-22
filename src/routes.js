a2e3a17566c5ef35de0100641f5a4d450dd23dee
eduvianna <eduardocosta_viana@live.com>
2019-10-22 19:02:40 -0300

    Configuração server

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+import UserController from './app/controllers/UserController';
+
+const routes = new Router();
+
+routes.post('/register', UserController.store);
+
+export default routes;
